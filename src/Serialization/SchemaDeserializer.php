6b2dc00c5311fada92ca5ae1cb2fc9252fe9f59d
Marius Hoch <mail@mariushoch.de>
2023-07-25 11:48:50 +0200

    Serialization: Remove cruft, add stubs

--- src/Serialization/SchemaDeserializer.php
+++ src/Serialization/SchemaDeserializer.php
@@ -4,20 +4,8 @@ declare( strict_types = 1 );
 
 namespace Wikibase\Schema\Serialization;
 
-use Deserializers\Deserializer;
-use Deserializers\Exceptions\DeserializationException;
 use Deserializers\TypedObjectDeserializer;
-use Wikibase\DataModel\Deserializers\TermDeserializer;
-use Wikibase\DataModel\Deserializers\TermListDeserializer;
-use Wikibase\DataModel\Entity\ItemId;
-use Wikibase\DataModel\Statement\StatementList;
-use Wikibase\DataModel\Term\TermList;
-use Wikibase\Lexeme\Domain\Model\Form;
-use Wikibase\Lexeme\Domain\Model\FormId;
-use Wikibase\Lexeme\Domain\Model\FormSet;
-use Wikibase\Lexeme\Domain\Model\Lexeme;
-use Wikibase\Lexeme\Domain\Model\LexemeId;
-use Wikibase\Lexeme\Domain\Model\SenseSet;
+use Wikibase\Schema\Domain\Model\Schema;
 
 /**
  * @license GPL-2.0-or-later
@@ -25,4 +13,8 @@ use Wikibase\Lexeme\Domain\Model\SenseSet;
  */
 class SchemaDeserializer extends TypedObjectDeserializer {
 
+	public function deserialize( $serialization ): Schema {
+		// TODO
+		return new Schema();
+	}
 }
--- src/Serialization/SchemaSerializer.php
+++ src/Serialization/SchemaSerializer.php
@@ -5,6 +5,20 @@ declare( strict_types = 1 );
 namespace Wikibase\Schema\Serialization;
 
 use Serializers\DispatchableSerializer;
+use Wikibase\Schema\Domain\Model\Schema;
 
 class SchemaSerializer implements DispatchableSerializer {
+
+	/**
+	 * @param mixed $object
+	 *
+	 * @return boolean
+	 */
+	public function isSerializerFor( $object ) {
+		return $object instanceof Schema;
+	}
+
+	public function serialize( $object ) {
+		// TODO
+	}
 }
